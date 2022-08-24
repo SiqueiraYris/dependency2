311197eb15369b209155438ac863040dc3e5eb56
Yris Siqueira <>
2022-08-24 13:03:57 -0300

    First commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Dependency2",
+    products: [
+        .library(
+            name: "Dependency2",
+            targets: ["Dependency2"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Dependency2",
+            dependencies: [],
+        resources: [
+            .process("test")
+        ]),
+        .testTarget(
+            name: "Dependency2Tests",
+            dependencies: ["Dependency2"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dependency2
+
+A description of this package.
